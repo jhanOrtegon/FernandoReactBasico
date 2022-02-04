793620938ddd525704139086e9b0cc459dda21be
jhan Carlos Ortegon <jhancarlos.ortegon@gmail.com>
2022-02-04 09:23:15 -0500

    nuevos cambios

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
 
+    const [counter, setCounter] = useState(0);
+
     const sumarUno = (e) => {
-        console.log(e);
+        setCounter(counter + 1)
     }
 
     return (
         <>
             <h1>Counter App</h1>
+            <h2>{counter}</h2>
             <h2>{value}</h2>
             <hr />
             <button onClick={sumarUno}>+1</button>
