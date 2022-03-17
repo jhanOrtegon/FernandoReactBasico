43192735551f6f023d04c6baca8bfbf623e03350
jhan Carlos Ortegon <jhancarlos.ortegon@gmail.com>
2022-03-17 10:10:12 -0500

    app de heroes casi terminada

--- 06-heroes-app/src/routers/AppRouter.jsx
+++ 06-heroes-app/src/routers/AppRouter.jsx
@@ -1,14 +1,26 @@
 import { Login } from '../components/login/Login';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { DashboardRouter } from './DashboardRouter';
+import PrivateRouters from './PrivateRouters';
+import PublicRouters from './PublicRouters';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/login' element={<Login/>} exact/>
-            <Route path='/*' element={<DashboardRouter/>} exact/>
-        </Routes>
+      <Routes>
+        <Route path='/login' element={
+          <PublicRouters>
+            <Login />
+          </PublicRouters>
+
+        } exact />
+
+        <Route path='/*' element={
+          <PrivateRouters>
+            <DashboardRouter />
+          </PrivateRouters>}
+          exact />
+      </Routes>
     </BrowserRouter>
   )
 }
